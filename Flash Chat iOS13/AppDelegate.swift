6b39abf542f26c477993f627a91e36f4e789b94e
KosukeNakagawa <kosukenakagawa730@gmail.com>
2023-07-01 23:00:59 +0900

    complete192

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -11,6 +11,7 @@ import Firebase
 import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,6 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        let db = Firestore.firestore()
+        IQKeyboardManager.shared.enable = true
         return true
     }
 
